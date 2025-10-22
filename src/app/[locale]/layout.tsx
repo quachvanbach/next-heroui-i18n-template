44c38335bfac688ce8a410c247c5c3755f2bffcd
quachvanbach <quachvanbach@gmail.com>
2025-10-22 04:27:52 +0000

    Implement i18n functionality with next-intl for multi-language support

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,81 @@
+import "@/src/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@heroui/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/src/config/site";
+import { fontSans } from "@/src/config/fonts";
+import { Navbar } from "@/src/components/navbar";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { routing } from "@/src/lib/i18n/routing";
+import { notFound } from "next/navigation";
+import { getMessages } from "next-intl/server";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html suppressHydrationWarning lang={locale}>
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen text-foreground bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <NextIntlClientProvider messages={messages} locale={locale}>
+            <div className="relative flex flex-col h-screen">
+              <Navbar />
+              <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+                {children}
+              </main>
+              <footer className="w-full flex items-center justify-center py-3">
+                <Link
+                  isExternal
+                  className="flex items-center gap-1 text-current"
+                  href="https://heroui.com?utm_source=next-app-template"
+                  title="heroui.com homepage"
+                >
+                  <span className="text-default-600">Powered by</span>
+                  <p className="text-primary">HeroUI</p>
+                </Link>
+              </footer>
+            </div>
+          </NextIntlClientProvider>
+        </Providers>
+      </body>
+    </html>
+  );
+}
